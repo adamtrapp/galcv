e2d72b1fdedebd6e92db9c1b57092125d6b24db8
Adam <atrapp@astro.ucla.edu>
2020-08-12 11:49:38 -0700

    Initial Commit of supplementary files

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+name = 'galcv'
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setuptools.setup(name=name,
+      version='1.0.0-alpha.2',
+      description='A simple calculator for cosmic variance',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+      ],
+      keywords='Cosmology Cosmic Variance Galaxies',
+      url='https://github.com/adamtrapp/galcv',
+      author='Adam Trapp',
+      author_email='atrapp@astro.ucla.edu',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy','pandas'],
+      python_requires='~=3.3',
+      include_package_data=True,
+      zip_safe=False)
